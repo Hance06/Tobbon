36e35ca213487fe6c16ad45c534abd2f6e83cd2c
Halil Arslan <halilarslan5006@gmail.com>
2022-06-05 23:43:25 +0300

    Düzeltme

--- core/settings/base.py
+++ core/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8n#(gwge&4#0g2uwf^cgy+dw(yetrpeq2=r=r^t1fwlx7(4bbh'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG
