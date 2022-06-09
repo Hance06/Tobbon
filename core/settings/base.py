316d9c5b25f7c2d4223685487d4363315fbb0a02
Halil Arslan <halilarslan5006@gmail.com>
2022-06-09 17:15:39 +0300

    Update base.py

--- core/settings/base.py
+++ core/settings/base.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tobbon',
+    'django_extensions',
+    'rest_framework',
+    'tobbon.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
