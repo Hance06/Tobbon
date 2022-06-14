1d40f02e1c5ea23eb1d1bb790664825384d466d9
Halil Arslan <halilarslan5006@gmail.com>
2022-06-15 01:57:20 +0300

    Update development.py

--- core/settings/development.py
+++ core/settings/development.py
@@ -3,8 +3,6 @@ from core.settings.base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
