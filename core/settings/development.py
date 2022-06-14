35112f780d49e05f81a31d31582b14ec527c14a5
Halil Arslan <halilarslan5006@gmail.com>
2022-06-15 01:44:44 +0300

    Settings

--- core/settings/development.py
+++ core/settings/development.py
@@ -1,7 +1,7 @@
 from core.settings.base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
